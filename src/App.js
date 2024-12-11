57f1fa704e6d9b38e349a8412cf564604eec8797
ajay kumar reddy <kalakoti.reddy@coforge.com>
2024-12-11 09:58:22 +0530

    Displaying dealer and product details

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';//imrr
+
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {faBomb,faCoffee,faCameraRetro,faHome,faPeopleGroup} from  '@fortawesome/free-solid-svg-icons';
+import NavBar from './components/NavBar';
+import Product from './components/Product';
+import About from './components/About';
+import { HomePage } from './components/HomePage';
+import DealerProfile from './components/DealerProfile';
+library.add(faBomb,faCoffee,faCameraRetro,faHome,faPeopleGroup);
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>ProductHive</h1> 
+      </header>
+
+      <section>
+        <div style={{ backgroundImage: "url(/images/bg1.avif)",
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize:'cover', minHeight:'140vh',minWidth:'95vw'}}> 
+                    <Router>
+                      <NavBar></NavBar>
+                       <Routes> 
+                        
+                       <Route path='/' element={<HomePage/>}></Route>
+
+                        <Route path='/about' element={<About/>}></Route>
+
+                        <Route path='/products' element={<Product/>}></Route>
+                        <Route path='/dealers' element={<DealerProfile/>}></Route>
+
+
+
+
+
+                        
+                       </Routes>
+                    </Router>
+          </div>
+      </section>
+        <footer className="footer">
+          <p>&copy;All Rights Reserved to Producthive.com </p>
+        </footer>
+    </div>
+  );
+}
+
+export default App;
